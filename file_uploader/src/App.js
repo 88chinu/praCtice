fb2431c54d5eede0e1657377a11fa74e00761625
88chinu <chinmaya.pa42@gmail.com>
2025-01-02 16:37:00 +0000

    create a file uplaoder site

--- file_uploader/src/App.js
+++ file_uploader/src/App.js
@@ -44,3 +44,19 @@ app.delete('/delete/:fileName', (req, res) => {
   }
 });
 
+app.get('/view', (req, res) => {
+  const uploadDirectory = path.join(__dirname, 'filestorage');
+  fs.readdir(uploadDirectory, (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading the upload directory.');
+    } else {
+      res.json({ files });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
